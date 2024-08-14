a0bbe72ab665688d08c638d3a40e254a13528e9e
Scorhenao <samcorhenao@gmail.com>
2024-08-13 22:35:40 -0400

    required to update an user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail } from 'sequelize-typescript';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    email?: string; 
+    password?: string;
+}
