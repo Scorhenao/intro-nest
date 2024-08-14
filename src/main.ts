ca4a421613b76506478c5a575441ef2c40c42089
Scorhenao <samcorhenao@gmail.com>
2024-08-13 21:28:16 -0400

    adding global enpoint to the api REST

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false}); // the abortOnError in false allow to return an error messsage
+  app.setGlobalPrefix('api'); // the global enpoint to escificate the api rest
   await app.listen(3000);
 }
 bootstrap();
